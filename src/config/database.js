b2b819338b12ae2ef6f212446a7e3eb9275f1b9f
Nilton Atanazzio <nii.cesar@hotmail.com>
2022-11-01 15:25:17 -0300

    heroku_mongo

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URL
+const uri = process.env.MONGODB_URI
 
 const connect = async () => {
    try {
