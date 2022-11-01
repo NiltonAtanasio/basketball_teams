b8bba902c51e4449b278b510ca1f8ebd9fc89046
Nilton Atanazzio <nii.cesar@hotmail.com>
2022-11-01 14:54:52 -0300

    heroku_mongo

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose');
-
-const MONGODB_URL = 'mongodb://localhost:27017/tera'
+const MONGODB_URL = "mongodb+srv://atanazzio:m4st3rk3y@cluster0.q2frexl.mongodb.net/?retryWrites=true&w=majority";
 
 const connect = async () => {
-  try {
-    await mongoose.connect(MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    
-    console.log("DataBase connection sucessful")
-  } catch (error) {
-    console.error('Erro', error.message)
-  }
+   try {
+     await mongoose.connect(MONGODB_URL, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true
+     })
+  
+     console.log("DataBase connection sucessful")
+   } catch (error) {
+     console.error('Erro', error.message)
+   }
 }
 
 module.exports = {
